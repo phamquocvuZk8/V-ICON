b9c80e218a31116c84e547ab6be9ba5fbed537f1
phamquocvuZk8 <quocvupham6789@gmail.com>
2022-09-25 14:15:57 +0700

    fix port deploy heroku

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
-const path = require ('path');
-const fs = require('fs');
-const os = require('os');
-const events = require('events');
-var ham = new events.EventEmitter();
+const express = require('express')
+const app = express();
+const path = require('path')
 
-///path//
-// console.log(path.basename('C:/Users/This PC/Documents/Code/index.js'));/////Xuất ra : index.js
-// console.log(path.dirname('C:/Users/This PC/Documents/Code/index.js'));////Xuất ra : C:/Users/This PC/Documents/Code
-// console.log(path.parse('C:/Users/This PC/Documents/Code/index.js'));
-// console.log(path.join('C:/Users/This PC/Documents/Code/index.js'));////xuất ra : C:/Users/This PC/Documents/Code/index.js
+app.use('/congkhai', express.static(path.join(__dirname, './public')))
+app.use('/img', express.static(path.join(__dirname, './img')))
+app.get('/', (req, res ,next)=>{
+    res.sendFile(path.join(__dirname, 'hello.html'))
+})
 
-
-/////fs///////
-
-// console.log(fs.readdirSync('./')); ////liệt kê
-// console.log(fs.readdir('@', function(err, err2){
-//     console.log('error');
-// }));
-
-//////////os/////////
-
-// console.log(os.hostname());
-// console.log(os.totalmem());
-
-/////////events
-var g = ham.on('error', function(){
-        var c = 10;
-        var d =10;
-        function so(a,b){
-            return a + b;
-        }
-        var f = so(c,d);
-        console.log(f);
-    });
-
-/////////http
+app.listen(8000, (req, res, next)=>{
+})
