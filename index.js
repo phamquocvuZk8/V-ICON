7a0e380ceaba3ca55bca73a1a55cc588cfe3b0ab
phamquocvuZk8 <quocvupham6789@gmail.com>
2022-09-26 18:06:21 +0700

    fix port deploy heroku

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+const BodyParser = require('body-parser')
+const Account2Model = require("./Mongoose/model")
 
-app.use('/congkhai', express.static(path.join(__dirname, './public')))
-app.use('/img', express.static(path.join(__dirname, './img')))
-app.get('/', (req, res ,next)=>{
-    res.sendFile(path.join(__dirname, 'hello.html'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.get('/create',(req, res, next)=>{
+    res.sendFile(path.join(__dirname,'duan.html'))
+})
+app.post('/create',(req, res, next)=>{
+    var username = req.body.username
+    var password = req.body.password
+    Account2Model.findOne({
+        username:username,
+        password : password
+    })
+    .then((data)=>{
+        if(data){
+            res.sendFile(path.join(__dirname, 'hello.html'))
+        }else{
+            Account2Model.create({
+                username :username,
+                password :password
+            })
+            .then((data)=>{
+                res.sendFile
+            })
+        }
+    })
 })
 
 app.listen(process.env.PORT, (req, res, next)=>{
