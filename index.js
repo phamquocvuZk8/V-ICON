aec129fe1e8827553d4cc5b2bad79c3e4f381415
phamquocvuZk8 <quocvupham6789@gmail.com>
2022-09-28 18:05:08 +0700

    sua

--- index.js
+++ index.js
@@ -2,34 +2,53 @@ const express = require('express')
 const app = express();
 const path = require('path')
 const bodyParser = require('body-parser')
-const Account2Model = require("./Mongoose/model")
+const Account2Model = require("./Mongoose/model");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
-app.get('/create',(req, res, next)=>{
-    res.sendFile(path.join(__dirname,'duan.html'))
-})
+// app.get('/create',(req, res, next)=>{
+//     res.sendFile(path.join(__dirname,'duan.html'))
+// })
 app.post('/create',(req, res, next)=>{
     var username = req.body.username
     var password = req.body.password
-    Account2Model.findOne({
-        username:username,
+    Account2Model.find({
+        username: username,
         password : password
     })
     .then((data)=>{
         if(data){
-            res.json('đã có tài khoản')
+            res.status(404).json('đã có tài khoản')
         }else{
-            Account2Model.create({
+            Account2Model.createOne({
                 username :username,
                 password :password
             })
             .then((data)=>{
-                res.sendFile(path.join(__dirname, 'hello.html'))
+                res.json('thành công')
+            })
+            .catch((err)=>{
+                res.status(404).json('lỗi')
             })
         }
     })
 })
+app.post('/Login', (req, res, next)=>{
+    var username = req.body.username;
+    var password = req.body.password;
+    Account2Model.findOne({
+        username : username,
+        password : password
+    })
+    .then((data)=>{
+        res.json('đăng nhập thành công')
+    })
+    .catch((err)=>{
+        res.status(404).json('Lỗi')
+    })
+})
 
-app.listen(process.env.PORT, (req, res, next)=>{
+app.listen(8008, (req, res, next)=>{
+    console.log('sever running.......')
 })
