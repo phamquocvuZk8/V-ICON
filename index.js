558eb4df03e1799c645e597fcafb314f46bec3f1
phamquocvuZk8 <quocvupham6789@gmail.com>
2022-10-01 17:22:29 +0700

    sua

--- index.js
+++ index.js
@@ -1,54 +1,12 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const path = require('path')
-const bodyParser = require('body-parser')
-const Account2Model = require("./Mongoose/model");
+const path = require('path');
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
-app.get('/create',(req, res, next)=>{
-    res.sendFile(path.join(__dirname,'duan.html'))
+app.use('/public', express.static(path.join(__dirname, './public')))
+app.get('/',(req, res , next)=>{
+    res.sendFile(path.join(__dirname, 'duan.html'))
 })
-app.post('/create',(req, res, next)=>{
-    var username = req.body.username
-    var password = req.body.password
-    Account2Model.find({
-        username: username,
-        password : password
-    })
-    .then((data)=>{
-        if(data){
-            res.status(404).json('đã có tài khoản')
-        }else{
-            Account2Model.createOne({
-                username :username,
-                password :password
-            })
-            .then((data)=>{
-                res.json('thành công')
-            })
-            .catch((err)=>{
-                res.status(404).json('lỗi')
-            })
-        }
-    })
-})
-app.post('/Login', (req, res, next)=>{
-    var username = req.body.username;
-    var password = req.body.password;
-    Account2Model.findOne({
-        username : username,
-        password : password
-    })
-    .then((data)=>{
-        res.json('đăng nhập thành công')
-    })
-    .catch((err)=>{
-        res.status(404).json('Lỗi')
-    })
-})
-
-app.listen(process.env.PORT, (req, res, next)=>{
-    console.log('sever running.......')
+app.listen(8000, (req , res , next)=>{
+    console.log('Sever running...........')
 })
