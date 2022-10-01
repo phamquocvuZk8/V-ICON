fe88eb249f2c57fe46e8b840d4f04b585971d63d
phamquocvuZk8 <quocvupham6789@gmail.com>
2022-10-01 17:26:27 +0700

    sua lan 2

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.use('/public', express.static(path.join(__dirname, './public')))
 app.get('/',(req, res , next)=>{
     res.sendFile(path.join(__dirname, 'duan.html'))
 })
-app.listen(8000, (req , res , next)=>{
+app.listen(process.env.PORT, (req , res , next)=>{
     console.log('Sever running...........')
 })
